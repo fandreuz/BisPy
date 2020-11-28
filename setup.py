6b020ec5bc94e010a81605c39d8bbcff17f93ec5
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-11-28 21:56:02 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bisimulation_algorithms",
+    version="0.0.1",
+    author="Francesco Andreuzzi",
+    author_email="andreuzzi.francesco@gmail.com",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    license="LICENSE",
+    description="Python implementation of some algorithms for the computation of the maximum bisimulation",
+)
