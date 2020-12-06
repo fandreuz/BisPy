82f916b74e6ca1601e1d50d6f5603e7442cdde5d
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-06 23:02:49 +0100

    add leading underscore to private constants

--- src/dovier_fba/well_foundedness.py
+++ src/dovier_fba/well_foundedness.py
@@ -1,11 +1,11 @@
 import networkx as nx
 
 # a node which hasn't been visited yet
-WHITE = 10
+_WHITE = 10
 # a node whose neighborood is being visited in the current recursion tree of the algorithm
-GRAY = 11
+_GRAY = 11
 # a node which has already been visited
-BLACK = 12
+_BLACK = 12
 
 def dfs_wf_visit(graph: nx.Graph, current_node: int):
     """A recursive step of the algorithm for the well-foundedness of a graph. Visits all the neighboroods of current_node (DFS) and then mark it as visited.
@@ -15,24 +15,24 @@ def dfs_wf_visit(graph: nx.Graph, current_node: int):
         current_node (int): The node which is being visited (must be accessible with graph.nodes[current_node]).
     """
 
-    if graph.nodes[current_node]["color"] == BLACK:
+    if graph.nodes[current_node]["color"] == _BLACK:
         return
 
-    graph.nodes[current_node]["color"] = GRAY
+    graph.nodes[current_node]["color"] = _GRAY
 
     for destination in graph.adj[current_node]:
         if not graph.nodes[destination]["wf"]:
             graph.nodes[current_node]["wf"] = False
-        elif graph.nodes[destination]["color"] == GRAY:
+        elif graph.nodes[destination]["color"] == _GRAY:
             graph.nodes[current_node]["wf"] = False
             graph.nodes[destination]["wf"] = False
-        elif graph.nodes[destination]["color"] == WHITE:
+        elif graph.nodes[destination]["color"] == _WHITE:
             dfs_wf_visit(graph, destination)
             # se la chiamata ricorsiva scopre che destination è nwf, anche current_node è nwf
             if not graph.nodes[destination]["wf"]:
                 graph.nodes[current_node]["wf"] = False
 
-    graph.nodes[current_node]["color"] = BLACK
+    graph.nodes[current_node]["color"] = _BLACK
 
 
 def mark_wf_nodes(graph: nx.Graph):
@@ -46,10 +46,10 @@ def mark_wf_nodes(graph: nx.Graph):
     # intialize the keys
     for node in graph.nodes:
         graph.nodes[node]["wf"] = True
-        graph.nodes[node]["color"] = WHITE
+        graph.nodes[node]["color"] = _WHITE
 
     for node in graph.nodes:
-        if graph.nodes[node]["color"] == WHITE:
+        if graph.nodes[node]["color"] == _WHITE:
             dfs_wf_visit(graph, node)
 
     # remove the key 'color'
