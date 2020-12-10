00ca4f84d189f4edc53c9c23d4e69c354a455c49
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-10 22:06:23 +0100

    initial partition argument for pta_algorithm.rscp is now optional

--- src/paige_tarjan/pta_algorithm.py
+++ src/paige_tarjan/pta_algorithm.py
@@ -2,7 +2,10 @@ import networkx as nx
 import paige_tarjan.graph_decorator as decorator
 import paige_tarjan.pta as pta
 
-def rscp(graph: nx.Graph, initial_partition: list[list[int]]):
+def rscp(graph: nx.Graph, initial_partition: list[list[int]] = None):
+    if initial_partition == None:
+        initial_partition = list(range(len(graph.nodes)))
+
     (q_partition, _) = decorator.initialize(graph, initial_partition)
     rscp = pta.pta(q_partition)
     return [tuple(sorted(tp)) for tp in rscp]
