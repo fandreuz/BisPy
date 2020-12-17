f3c168f3d60548ac1f8eba073be0810367d4cc91
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-17 01:14:42 +0100

    fba representation of the input graph

---
+++ src/dovier_piazza_policriti/graph_decorator.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import dovier_piazza_policriti.graph_entities as entities
+from typing import List
+
+
+def to_normal_graph(graph: nx.Graph) -> List[entities.Vertex]:
+    vertexes_counterimage = [[] for _ in range(graph.nodes)]
+    vertexes_image = [[] for _ in range(graph.nodes)]
+
+    # build the image/counterimage
+    for edge in graph.edges:
+        vertexes_image[edge[0]].append(edge[1])
+        vertexes_counterimage[edge[1]].append(edge[0])
+
+    # initialize the new representations
+    return [
+        entities.Vertex(
+            graph.nodes[node]["rank"],
+            neighborhoods=vertexes_image,
+            counterimage=vertexes_counterimage[node],
+        )
+        for node in graph.nodes
+    ]
