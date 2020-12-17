8f24d342f4e29bfe381d1416c78a83c301d52e11
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-17 20:10:45 +0100

    test prepare_graph

--- tests/fba/algorithm/test_fba.py
+++ tests/fba/algorithm/test_fba.py
@@ -1,6 +1,7 @@
 import pytest
 import dovier_piazza_policriti.fba as fba
 import dovier_piazza_policriti.graph_entities as entities
+import tests.fba.algorithm.fba_test_cases as test_cases
 
 @pytest.mark.parametrize(
     "rank, expected",
@@ -11,6 +12,14 @@ def test_rank_to_partition_idx(rank, expected):
 
 @pytest.mark.parametrize(
     "block, expected",
+    zip(test_cases.block_counterimage_cases, test_cases.block_counterimage_expected)
 )
 def test_build_block_counterimage(block, expected):
     assert set(fba.build_block_counterimage(block)) == set(expected)
+
+@pytest.mark.parametrize(
+    "graph, expected",
+    zip(test_cases.prepare_graph_cases, test_cases.prepare_graph_expected)
+)
+def test_prepare_graph(graph, expected):
+    assert set(fba.prepare_graph(graph)) == set(expected)
