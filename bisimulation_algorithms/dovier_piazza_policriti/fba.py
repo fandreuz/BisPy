1bf8a77feefc9456443f35d71b950b88cc919ad5
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-17 15:11:56 +0100

    introduced Block and Vertex entities. split is now done calculating the block counterimage. docstring

--- bisimulation_algorithms/dovier_piazza_policriti/fba.py
+++ bisimulation_algorithms/dovier_piazza_policriti/fba.py
@@ -1,40 +1,147 @@
 import networkx as nx
-
-from .rank import compute_rank
-from ..paige_tarjan.pta_algorithm import rscp as paige_tarjan
-
-
-def collapse(graph, blocks: list[list]):
-    if len(nodes) > 0:
-        keep_node = list(nodes)[0]
-
-        for node in nodes:
-            if node == keep_node:
-                pass
-            else:
-                incident_edges = list(
-                    filter(
-                        lambda edge: edge[1] == node or edge[0] == node,
-                        graph.edges,
-                    )
-                )
-
-                # replace edges incident to node with eges incident to
-                # keep_node
-                graph.add_edges_from(
-                    map(
-                        lambda edge: (keep_node, edge[1])
-                        if edge[0] == node
-                        else (edge[0], keep_node),
-                        incident_edges,
-                    )
-                )
-
-                graph.remove_node(node)
+import dovier_piazza_policriti.rank as rank
+import paige_tarjan.pta_algorithm as pta
+import dovier_piazza_policriti.graph_entities as entities
+from typing import Iterable, List
+from itertools import islice
+import dovier_piazza_policriti.graph_decorator as decorator
 
 
-def split2(graph, node, partition, from_index):
-    pass
+def collapse(block: Iterable[entities._Vertex]) -> entities._Vertex:
+    # prevent index exception
+    if len(block) > 0:
+        keep_node = list(block)[0]
+
+        # skip the first node
+        for vertex in islice(block, start=1):
+            vertex.collapsed_to = keep_node
+
+        return keep_node
+
+
+def build_block_counterimage(block: entities._Block) -> List[entities._Vertex]:
+    """Given a block B, construct a list of vertexes x such that x->y and y is in B.
+
+    Args:
+        block (entities._Block): A block.
+
+    Returns:
+        list[entities._Vertex]: A list of vertexes x such that x->y and y is in B (the order doesn't matter).
+    """
+
+    block_counterimage = []
+
+    for vertex in block.vertexes:
+        for counterimage_vertex in vertex.counterimage:
+            # this vertex should be added to the counterimage only if necessary (avoid duplicates)
+            if not counterimage_vertex.visited:
+                block_counterimage.append(counterimage_vertex)
+                # remember to release this vertex
+                counterimage_vertex.visit()
+
+    for vertex in block_counterimage:
+        # release this vertex so that it can be visited again in a next splitting phase
+        vertex.release()
+
+    return block_counterimage
+
+
+def rank_to_partition_idx(rank: int) -> int:
+    """Convert the rank of a block/vertex to its expected index in the list which represents the partition of nodes.
+
+    Args:
+        rank (int): The input rank (int or float('-inf'))
+
+    Returns:
+        int: The index in the partition of a block such that block.rank = rank
+    """
+
+    if rank == float("-inf"):
+        return 0
+    else:
+        return rank + 1
+
+
+def split_upper_ranks(partition: List[List[entities._Block]], block: entities._Block):
+    """Update the blocks of the partition whose rank is greater than block.rank, in order to make the partition stable with respect to block.
+
+    Args:
+        partition (List[List[entities._Block]]): The current partition.
+        block (entities._Block): The block the partition has to be stable with respect to.
+    """
+
+    block_counterimage = build_block_counterimage(block)
+
+    modified_blocks = []
+
+    for vertex in block_counterimage:
+        # if this is an upper-rank node with respect to the collapsed block, we can split it from its block
+        if vertex.rank > block.rank:
+            # if needed, create the aux block to help during the splitting phase
+            if vertex.block.aux_block == None:
+                vertex.block.aux_block = entities._Block(vertex.block.rank)
+                modified_blocks.append(vertex.block)
+
+            # remove the vertex in the counterimage from its current block
+            vertex.block.remove_vertex(vertex)
+            # put the vertex in the counterimage in the aux_block
+            vertex.block.aux_block.insert_vertex(vertex)
+
+    # insert the new blocks in the partition, and then reset aux_block for each modified node
+    for block in modified_blocks:
+        partition[rank_to_partition_idx(block.rank)].append(block.aux_block)
+        block.aux_block = None
+
+
+def prepare_graph(graph: nx.Graph) -> List[entities._Vertex]:
+    """Prepare the input graph for the algorithm. Computes the rank for each node, and then converts the graph to a usable representation.
+
+    Args:
+        graph (nx.Graph): The input graph
+
+    Returns:
+        List[entities._Vertex]: A convenient representation of the given graph (contains only nodes and edges).
+    """
+
+    rank.compute_rank(graph)
+    return decorator.to_normal_graph(graph)
+
+
+def create_subgraph_of_rank(
+    partition: List[List[entities._Block]], rank: int
+) -> nx.Graph:
+    """Creates a subgraph of nodes having the given rank, from the given partition.
+
+    Args:
+        partition (List[List[entities._Block]]): The current partition.
+        rank (int): Rank of the nodes in the subgraph.
+
+    Returns:
+        nx.Graph: The subgraph.
+    """
+
+    subgraph = nx.DiGraph()
+
+    if rank == float("-inf"):
+        idx = 0
+    else:
+        idx = rank + 1
+    blocks_rank_i = partition[idx]
+
+    vertexes = []
+    for block in blocks_rank_i:
+        for vertex in block:
+            image_rank_i = filter(
+                lambda image_vertex: image_vertex.rank == rank, vertex.image
+            )
+            subgraph.add_edges_from(
+                [(vertex, image_vertex) for image_vertex in image_rank_i]
+            )
+
+    subgraph.add_nodes_from(vertexes)
+
+    return subgraph
+
 
 
 def fba(graph: nx.Graph):
@@ -45,50 +152,42 @@ def fba(graph: nx.Graph):
         graph (nx.Graph): The input graph.
     """
 
-    compute_rank(graph)
-
-    # find the maximum rank
-    max_rank = float("-inf")
-    for node in graph.nodes:
-        max_rank = max(max_rank, graph.nodes[node]["rank"])
+    vertexes = prepare_graph(graph)
+    # find the maximum rank in the graph
+    max_rank = max(map(lambda vertex: vertex.rank, vertexes))
 
     # initialize the initial partition. the first index is for -infty
-    # partition contains is a list of lists, each sub-list contains the
-    # sub-blocks of nodes at the i-th rank
+    # partition contains is a list of lists, each sub-list contains the sub-blocks of nodes at the i-th rank
     if max_rank != float("-inf"):
-        partition = [[] for _ in range(max_rank + 2)]
+        partition = [[entities._Block()] for _ in range(max_rank + 2)]
     else:
         # there's a single possible rank, -infty
-        partition = [[]]
+        partition = [[entities._Block()]]
 
     # populate the blocks of the partition according to the ranks
-    for node in graph.nodes:
-        if graph.nodes[node]["rank"] == float("-inf"):
-            # rank is -infty, therefore we put this node in the first position
-            # of the list
-            partition_idx = 0
-        else:
-            # rank is not -infty
-            partition_idx = graph.nodes[node]["rank"] + 1
-
-        # put this node in the (only) list at partition_idx in partition
-        # (there's only one block for each rank at the moment in the partition)
-        partition[partition_idx][0].add(node)
+    for vertex in vertexes:
+        # put this node in the (only) list at partition_idx in partition (there's only one block for each rank at the moment in the partition)
+        partition[rank_to_partition_idx(vertex.rank)][0].insert_vertex(vertex)
 
     # collapse B_{-infty}
     if len(partition[0]) > 0:
-        # pass the blocks of rank -infty to collapse
-        collapse(graph, partition[0])
-        # extract the first node of the first list of the first index in
-        # position
-        survivor_node = partition[0][0][0]
+        # there's only one block in partition[0] (B_{-infty}) at the moment, namely partition[0][0]
+        #survivor_node = collapse(partition[0][0])
+
         # update the partition
-        split2(graph, survivor_node, partition, 1)
+        split_upper_ranks(partition, first_rank_idx=1, collapsed_rank=float("-inf"))
 
     # loop over the ranks
     for i in range(1, max_rank + 2):
-        paige_tarjan()
-        collapse(graph, partition[i])
-        split2(
-            graph,
-        )
+        # create the subgraph of rank i-1
+        subgraph = create_subgraph_of_rank(partition[i], i - 1)
+
+        # apply PTA to the subgraph at rank i
+        rscp = pta.rscp(subgraph, partition[i])
+
+        # collapse all the blocks in B_i
+        #for rscp_block in rscp:
+        #    collapse(rscp_block)
+
+        # update the partition
+        split_upper_ranks(partition, first_rank_idx=i + 1, collapsed_rank=i - 1)
--- src/dovier_piazza_policriti/graph_entities.py
+++ src/dovier_piazza_policriti/graph_entities.py
@@ -1,5 +1,5 @@
 from typing import Iterable
-
+from llist import dllist, dllistnode
 
 class _Vertex:
     """Represents a vertex of the input graph. The representation holds some key attributes used by the algorithm (O(1) access).
@@ -12,10 +12,38 @@ class _Vertex:
     """
 
     def __init__(
-        self, rank: int, neighborhoods: Iterable[Vertex], image: Iterable[Vertex]
+        self, rank: int, neighborhoods: Iterable, image: Iterable
     ):
         self.rank = rank
         self.counterimage = counterimage
         self.image = image
 
         self.collapsed_to = None
+        self.block = None
+        self.dllistnode = None
+
+class _Block:
+    def __init__(self, rank: int):
+        self.vertexes = dllist([])
+        self.aux_block = None
+
+        self.visited = False
+
+        self.rank = rank
+
+    def visit(self):
+        self.visited = True
+
+    def release(self):
+        self.visited = False
+
+    def size(self) -> int:
+        return len(self.vertexes)
+
+    def append_vertex(self, vertex: _Vertex):
+        vertex.block = self
+        vertex.dllistnode = self.vertexes.append(vertex)
+
+    def remove_vertex(self, vertex: _Vertex):
+        vertex.block = None
+        self.vertexes.remove(vertex.dllistnode)
