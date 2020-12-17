4730c7683e1a9a047443f32351850812772d94be
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-17 19:26:32 +0100

    _Vertex.image/counterimage now contains _Vertex instances

--- src/dovier_piazza_policriti/graph_decorator.py
+++ src/dovier_piazza_policriti/graph_decorator.py
@@ -4,20 +4,11 @@ from typing import List
 
 
 def to_normal_graph(graph: nx.Graph) -> List[entities._Vertex]:
-    vertexes_counterimage = [[] for _ in range(graph.nodes)]
-    vertexes_image = [[] for _ in range(graph.nodes)]
+    vertexes = [entities._Vertex(graph.nodes[node]['rank']) for node in graph.nodes]
 
     # build the image/counterimage
     for edge in graph.edges:
-        vertexes_image[edge[0]].append(edge[1])
-        vertexes_counterimage[edge[1]].append(edge[0])
+        vertexes[edge[0]].append_to_image(vertexes[edge[1]])
+        vertexes[edge[1]].append_to_counterimage(vertexes[edge[0]])
 
-    # initialize the new representations
-    return [
-        entities._Vertex(
-            graph.nodes[node]["rank"],
-            image=vertexes_image,
-            counterimage=vertexes_counterimage[node],
-        )
-        for node in graph.nodes
-    ]
+    return vertexes
