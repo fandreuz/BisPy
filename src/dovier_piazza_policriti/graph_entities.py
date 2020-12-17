04f5367b861a3eea2f56e903ccb496fd104a12cb
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-17 01:14:42 +0100

    fba representation of the input graph

--- src/dovier_piazza_policriti/graph_entities.py
+++ src/dovier_piazza_policriti/graph_entities.py
@@ -1,5 +1,15 @@
 from typing import Iterable
 
 class Vertex:
-    def __init__(self, adjacency: Iterable, ):
-        pass
+    """Represents a vertex of the input graph. The representation holds some key attributes used by the algorithm (O(1) access).
+
+    Attributes:
+        rank:          The rank of this vertex
+        counterimage:  A such that A -> self
+        neighborhoods: A such that self -> A
+    """
+
+    def __init__(self, rank: int, neighborhoods: Iterable[Vertex], counterimage: Iterable[Vertex]):
+        self.rank = rank
+        self.counterimage = counterimage
+        self.neighborhoods = neighborhoods
