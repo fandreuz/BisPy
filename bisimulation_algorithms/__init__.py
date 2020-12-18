f7ed7955c9360ff42ba7a834ddce9090a3417114
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-18 11:14:20 +0100

    src -> bisimulation_algorithms

---
+++ bisimulation_algorithms/__init__.py
@@ -0,0 +1,19 @@
+"""A Python package for computing the maximal bisimulation of directed graphs
+
+.. moduleauthor:: Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
+
+"""
+
+__author__ = "Francesco Andreuzzi"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Francesco Andreuzzi"]
+__license__ = "MIT"
+__release__ = "0.0"
+__subrelease__ = "1"
+__version__ = __release__+"."+__subrelease__
+__maintainer__ = "Francesco Andreuzzi"
+__email__ = "andreuzzi.francesco@gmail.com"
+__status__ = "Development"
+__name__ = "bisimulation_algorithms"
+
+from .paige_tarjan.pta_algorithm import rscp as paige_tarjan
