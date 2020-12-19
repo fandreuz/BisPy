0d099278c3c6b45939db666e68aafdb348270d80
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-19 08:36:50 +0100

    new version of rscp_utilities for fba

---
+++ tests/fba/rscp_utilities.py
@@ -0,0 +1,31 @@
+from bisimulation_algorithms.dovier_piazza_policriti.graph_entities import _Block, _Vertex
+
+def check_block_stability(
+    block1: _Block, block2: _Block
+) -> bool:
+    """Checks whether block1 is stable with respect to block2, namely block1
+    is a subset of E^{-1}(block2) or the two sets are distinct. This method
+    doesn't check whether block2 is stable with respect to block1.
+
+    Args:
+        block1 (_Block): The first block.
+        block12 (_Block): The second block.
+
+    Returns:
+        bool: True if block1 is stable with respect to block2, False otherwise.
+    """
+
+    # if there's a vertex y in B_qblock_vertexes such that for the i-th vertex
+    # we have i->y, then is_inside_B[i] = True
+    is_inside_B = []
+    for vertex in block1.vertexes:
+        flag = False
+        for image_vertex in vertex.image:
+            if image_vertex in block2:
+                flag = True
+        is_inside_B.append(flag)
+
+    # all == True if for each vertex x in A there's a vertex y such that
+    # x \in E({x}) AND y \in B.
+    # not any == True if the set "image of A" and B are distinct.
+    return all(is_inside_B) or not any(is_inside_B)
