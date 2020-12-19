b6ee6761fea191805156989c7e60f8d515829cc8
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-19 01:39:05 +0100

    test create subgraph of rank

--- tests/fba/algorithm/fba_test_cases.py
+++ tests/fba/algorithm/fba_test_cases.py
@@ -1,21 +1,22 @@
-import bisimulation_algorithms.dovier_piazza_policriti.graph_entities as entities
+from bisimulation_algorithms.dovier_piazza_policriti.graph_entities import _Block, _Vertex
+import bisimulation_algorithms.dovier_piazza_policriti.fba as fba
 import networkx as nx
 
 block_counterimage_cases = []
 block_counterimage_expected = []
 
 # 1
-vertexes1 = [entities._Vertex(label=i, rank=0) for i in range(5)]
+vertexes1 = [_Vertex(label=i, rank=0) for i in range(5)]
 for i in range(3, 5):
     vertexes1[0].append_to_counterimage(vertexes1[i])
 vertexes1[1].append_to_counterimage(vertexes1[2])
-block_counterimage_cases.append(entities._Block(0, [vertexes1[0], vertexes1[1]]))
+block_counterimage_cases.append(_Block(0, [vertexes1[0], vertexes1[1]]))
 block_counterimage_expected.append(vertexes1[2:5])
 
 # 2
-vertexes2 = [entities._Vertex(label=i, rank=0) for i in range(5)]
+vertexes2 = [_Vertex(label=i, rank=0) for i in range(5)]
 vertexes2[1].append_to_counterimage(vertexes2[2])
-block_counterimage_cases.append(entities._Block(0, [vertexes2[0], vertexes2[1]]))
+block_counterimage_cases.append(_Block(0, [vertexes2[0], vertexes2[1]]))
 block_counterimage_expected.append([vertexes2[2]])
 
 # ------------------------------------------------------
@@ -28,7 +29,36 @@ graph1 = nx.DiGraph()
 graph1.add_nodes_from(range(5))
 graph1.add_edges_from([(i, i + 1) for i in range(4)])
 prepare_graph_cases.append(graph1)
-expected1 = [entities._Vertex(label=i, rank=4 - i) for i in range(5)]
+expected1 = [_Vertex(label=i, rank=4 - i) for i in range(5)]
 for i in range(4):
     expected1[i].append_to_image(expected1[i + 1])
 prepare_graph_expected.append(expected1)
+
+# ------------------------------------------------------
+
+create_subgraph_cases = []
+create_subgraph_cases_rank = []
+create_subgraph_expected = []
+
+# 1
+graph1 = nx.DiGraph()
+graph1.add_nodes_from(range(5))
+graph1.add_edges_from((i, i + 1) for i in range(3))
+graph1.add_edge(3, 0)
+graph1.add_edge(4,0)
+
+create_subgraph_cases_rank.append(float('-inf'))
+create_subgraph_expected.append(graph1)
+create_subgraph_cases.append(fba.create_initial_partition(fba.prepare_graph(graph1))[0])
+
+# 2
+graph2 = nx.DiGraph()
+graph2.add_nodes_from(range(3))
+graph2.add_edges_from([(0, 1), (0, 2)])
+
+graph2e = nx.DiGraph()
+graph2e.add_nodes_from([1,2])
+
+create_subgraph_cases_rank.append(0)
+create_subgraph_expected.append(graph2e)
+create_subgraph_cases.append(fba.create_initial_partition(fba.prepare_graph(graph2))[1])
--- tests/fba/algorithm/test_fba.py
+++ tests/fba/algorithm/test_fba.py
@@ -2,6 +2,7 @@ import pytest
 import bisimulation_algorithms.dovier_piazza_policriti.fba as fba
 import bisimulation_algorithms.dovier_piazza_policriti.graph_entities as entities
 import tests.fba.algorithm.fba_test_cases as test_cases
+import networkx as nx
 
 @pytest.mark.parametrize(
     "rank, expected",
@@ -23,3 +24,14 @@ def test_build_block_counterimage(block, expected):
 )
 def test_prepare_graph(graph, expected):
     assert set(fba.prepare_graph(graph)) == set(expected)
+
+@pytest.mark.parametrize(
+    "blocks_at_rank, rank, expected_graph",
+    zip(test_cases.create_subgraph_cases, test_cases.create_subgraph_cases_rank, test_cases.create_subgraph_expected)
+)
+def test_create_subgraph_of_rank(blocks_at_rank, rank, expected_graph):
+    result = fba.create_subgraph_of_rank(blocks_at_rank, rank)
+
+    isomorphic = nx.is_isomorphic(result, expected_graph)
+    same_nodes = list(result.nodes) == list(expected_graph.nodes)
+    assert isomorphic and same_nodes
