fb96e00ae8296bb8ee31514ffd57357d314cbfc2
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-23 00:04:37 +0100

    extract graph normalization code from pta_algorithms.py, remains in pta.py

--- bisimulation_algorithms/paige_tarjan/pta.py
+++ bisimulation_algorithms/paige_tarjan/pta.py
@@ -1,7 +1,14 @@
 from llist import dllist, dllistnode
-from typing import List, Tuple
+from typing import List, Dict, Any, Tuple, Iterable
+import networkx as nx
 
 from .graph_entities import _Vertex, _XBlock, _QBlock, _Count
+from .graph_decorator import initialize
+
+from bisimulation_algorithms.utilities.graph_normalization import (
+    check_normal_integer_graph,
+    convert_to_integer_graph,
+)
 
 
 # choose the smallest qblock of the first two
@@ -317,3 +324,64 @@ def pta(q_partition: List[_QBlock]) -> List[Tuple]:
         tuple(map(lambda vertex: vertex.label, qblock.vertexes))
         for qblock in filter(lambda qblock: qblock.size > 0, q_partition)
     ]
+
+
+def rscp(
+    graph: nx.Graph,
+    initial_partition: Iterable[Iterable[int]] = None,
+    is_integer_graph: bool = False,
+) -> List[Tuple]:
+    """Compute the RSCP of the given graph, with the given initial partition.
+    This function needs to work with an integer graph (nodes represented by an
+    integer), therefore it checks this property before starting the
+    Paige-Tarjan algorithm, and creates an integer graph if it is not
+    statisfied.
+
+    Args:
+        graph (nx.Graph): The input graph.
+        initial_partition (Iterable[Iterable[int]], optional): The initial
+        partition for the given graph. Defaults to None.
+        is_integer_graph (bool, optional): If True, the function assumes that
+        the graph is integer, and skips the integrality check (may be useful
+        when performance is important). Defaults to False.
+
+    Returns:
+        List[Tuple]: The RSCP of the given (even non-integer) graph, with the
+        given initial partition.
+    """
+
+    # if True, the input graph is already an integer graph
+    original_graph_is_integer = is_integer_graph or check_normal_integer_graph(
+        graph
+    )
+
+    # if initial_partition is None, then it's the trivial partition
+    if initial_partition is None:
+        # only list(graph.nodes) isn't OK
+        initial_partition = [list(graph.nodes)]
+
+    if not original_graph_is_integer:
+        # convert the graph to an "integer" graph
+        integer_graph, node_to_idx = convert_to_integer_graph(graph)
+
+        # convert the initial partition to a integer partition
+        integer_initial_partition = [
+            [node_to_idx[old_node] for old_node in block]
+            for block in initial_partition
+        ]
+    else:
+        integer_graph = graph
+        integer_initial_partition = initial_partition
+
+    # compute the RSCP
+    (q_partition, _) = initialize(integer_graph, integer_initial_partition)
+    rscp = pta(q_partition)
+
+    if original_graph_is_integer:
+        return rscp
+    else:
+        # create a mapping from idx to the original nodes
+        idx_to_node = sorted(node_to_idx, key=lambda node: node_to_idx[node])
+
+        # compute the RSCP of the original graph
+        return [tuple(idx_to_node[idx] for idx in block) for block in rscp]
