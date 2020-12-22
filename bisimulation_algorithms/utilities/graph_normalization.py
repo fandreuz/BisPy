6bd05ec644c83503b658da300068d403e183315e
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-12-23 00:41:03 +0100

    improve comments. add missing import

--- bisimulation_algorithms/utilities/graph_normalization.py
+++ bisimulation_algorithms/utilities/graph_normalization.py
@@ -1,12 +1,14 @@
 import networkx as nx
-from typing import Dict, Tuple, Any
+from typing import Dict, Tuple, Any, List
+
 
 def convert_to_integer_graph(
     graph: nx.Graph,
 ) -> Tuple[nx.Graph, Dict[Any, int]]:
     """Convert the given graph to an isomorphic graph whose nodes are integer
     numbers. Moreover, creates a Dict which maps nodes of the original graph to
-     the corresponding node of the integer graph.
+     the corresponding node of the integer graph. Nodes in the graph have be
+     hashable objects.
 
     Args:
         graph (nx.Graph): The input graph.
@@ -47,3 +49,26 @@ def check_normal_integer_graph(graph: nx.Graph) -> bool:
         all(map(lambda node: isinstance(node, int) and node >= 0, graph.nodes))
         and max(graph.nodes) == len(graph.nodes) - 1
     )
+
+
+def back_to_original(
+    partition: List[Tuple[int]], node_to_idx: Tuple[nx.Graph, Dict[Any, int]]
+) -> List[Tuple[Any]]:
+    """Convert the given integer partition to a partition made by the original
+    elements of the graph, using the mapping node_to_idx.
+
+    Args:
+        partition (List[Tuple[int]]): The input partition.
+        node_to_idx (Tuple[nx.Graph, Dict[Any, int]]): The mapping node->idx
+        returned by convert_to_integer_graph.
+
+    Returns:
+        List[Tuple[Any]]: The given partition, represented with the symbols
+        (nodes) of the original graph.
+    """
+
+    # create a mapping from idx to the original nodes
+    idx_to_node = sorted(node_to_idx, key=lambda node: node_to_idx[node])
+
+    # compute the RSCP of the original graph
+    return [tuple(idx_to_node[idx] for idx in block) for block in partition]
