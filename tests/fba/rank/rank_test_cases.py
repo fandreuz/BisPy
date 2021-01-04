1bc5213f5487097f3e5b229aa2dbb509d71094fe
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-01-05 00:29:58 +0100

    refactor rank tests

--- tests/fba/rank/rank_test_cases.py
+++ tests/fba/rank/rank_test_cases.py
@@ -1,6 +1,7 @@
 import networkx as nx
 
 graphs = []
+noderank_dicts = []
 
 # 1
 graph1 = nx.DiGraph()
@@ -8,11 +9,15 @@ graph1.add_nodes_from(range(6))
 graph1.add_edges_from([(0, 1), (1, 2), (2, 3), (2, 4), (4, 5), (5, 2)])
 graphs.append(graph1)
 
+noderank_dicts.append({4: 1, 2: 1, 3: 0, 5: 1, 1: 1, 0: 1})
+
 # 2 : tree
 graph2 = nx.DiGraph()
 graph2.add_nodes_from(range(7))
 graph2.add_edges_from([(0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (5, 6)])
 graphs.append(graph2)
+
+noderank_dicts.append({3: 0, 2: 1, 1: 2, 6: 0, 5: 1, 4: 2, 0: 3})
 
 # 3: cycle
 graph3 = nx.DiGraph()
@@ -20,30 +25,16 @@ graph3.add_nodes_from(range(4))
 graph3.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 0)])
 graphs.append(graph3)
 
-graphs_noderankdict = []
-
-# 1
-graphs_noderankdict.append((graph1, {4: 1, 2: 1, 3: 0, 5: 1, 1: 1, 0: 1}))
-
-# 2 : tree
-graphs_noderankdict.append((graph2, {3: 0, 2: 1, 1:2, 6: 0, 5: 1, 4: 2, 0: 3}))
-
 # 3: cycle
-graphs_noderankdict.append((graph3, {
-    0: float('-inf'),
-    1: float('-inf'),
-    2: float('-inf'),
-    3: float('-inf')
-}))
+noderank_dicts.append(
+    {0: float("-inf"), 1: float("-inf"), 2: float("-inf"), 3: float("-inf")}
+)
 
-# 3: edge to G-SCC leaf
+# 4: edge to G-SCC leaf
 graph4 = nx.DiGraph()
 graph4.add_nodes_from(range(4))
 graph4.add_edges_from([(0, 1), (1, 2), (2, 0), (3, 0)])
 graphs.append(graph4)
-graphs_noderankdict.append((graph4, {
-    0: float('-inf'),
-    1: float('-inf'),
-    2: float('-inf'),
-    3: float('-inf')
-}))
+noderank_dicts.append(
+    {0: float("-inf"), 1: float("-inf"), 2: float("-inf"), 3: float("-inf")}
+)
--- tests/fba/rank/test_rank.py
+++ tests/fba/rank/test_rank.py
@@ -5,10 +5,10 @@ from bisimulation_algorithms.dovier_piazza_policriti.rank_computation import (
     compute_rank,
 )
 from bisimulation_algorithms.dovier_piazza_policriti.graph_decorator import prepare_graph
-from .rank_test_cases import graphs_noderankdict, graphs
+from .rank_test_cases import noderank_dicts, graphs
 
 
-@pytest.mark.parametrize("graph, node_rank_dict", graphs_noderankdict)
+@pytest.mark.parametrize("graph, node_rank_dict", zip(graphs, noderank_dicts))
 def test_compute_rank(graph, node_rank_dict: dict):
     print(graph.edges)
     vertexes = prepare_graph(graph)
