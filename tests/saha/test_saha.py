858ffaab7a3e9ed53e0b06b5e4a0fb534c3b6bca
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-01-29 01:49:34 +0100

    test find_vertexes

--- tests/saha/test_saha.py
+++ tests/saha/test_saha.py
@@ -6,7 +6,7 @@ from bisimulation_algorithms.dovier_piazza_policriti.graph_decorator import (
 from bisimulation_algorithms.dovier_piazza_policriti.fba import (
     create_initial_partition,
 )
-from bisimulation_algorithms.saha.saha import check_old_blocks_relation
+from bisimulation_algorithms.saha.saha import check_old_blocks_relation, find_vertexes
 
 def test_check_old_blocks_relation():
     graph = nx.DiGraph()
@@ -22,3 +22,13 @@ def test_check_old_blocks_relation():
     assert check_old_blocks_relation(vertexes[2], vertexes[3])
     assert not check_old_blocks_relation(vertexes[0], vertexes[4])
     assert not check_old_blocks_relation(vertexes[4], vertexes[0])
+
+def test_find_vertex():
+    graph = nx.DiGraph()
+    graph.add_nodes_from(range(5))
+    graph.add_edges_from([(0,1), (1,2), (2,3), (4,1)])
+
+    vertexes = prepare_graph(graph)
+    partition = create_initial_partition(vertexes)
+
+    assert find_vertexes(partition, 0,1) == (vertexes[0], vertexes[1])
