52abc2eec4da0660889456f75eb5158ababa9e34
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-02-05 01:54:40 +0100

    new test case

--- tests/pta/pta_test_cases.py
+++ tests/pta/pta_test_cases.py
@@ -457,6 +457,22 @@ graph_partition_rscp_tuples.append(
     )
 )
 
+# 12
+graph = nx.DiGraph()
+graph.add_nodes_from(range(5))
+graph.add_edges_from(
+    [
+        (0,1),(1,2),(2,3),(3,0),(0,4)
+    ]
+)
+graph_partition_rscp_tuples.append(
+    (
+        graph,
+        [(0,1,2,3,4)],
+        set([(0,), (1,), (2,), (3,), (4,)]),
+    )
+)
+
 def build_full_graphs(num_of_nodes):
         graph = nx.DiGraph()
         for i in range(num_of_nodes):
