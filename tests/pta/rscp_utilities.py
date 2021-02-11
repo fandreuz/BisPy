718a3f7345b2dcf23f01750a8b9605b4de63bae6
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-02-12 00:17:34 +0100

    added functions for set of _Vertex

--- tests/pta/rscp_utilities.py
+++ tests/pta/rscp_utilities.py
@@ -1,12 +1,11 @@
 import itertools
 from typing import List, Iterable
 
-from bisimulation_algorithms.utilities.graph_entities import _Vertex
-
+from bisimulation_algorithms.utilities.graph_entities import _Vertex, _QBlock
 
 # check if the given partition is stable with respect to the given block, or if
 # it's stable if the block isn't given
-def is_stable_partition(partition: List[List[_Vertex]]) -> bool:
+def is_stable_vertexes_partition(partition: List[List[_Vertex]]) -> bool:
     """Checks the stability of the given partition. The input must be a
     partition of Vertex instances, and the relation which we consider for the
     stability is a->b, where a,b are two vertexes.
@@ -20,16 +19,22 @@ def is_stable_partition(partition: List[List[_Vertex]]) -> bool:
 
     for couple in itertools.product(partition, repeat=2):
         if not (
-            check_block_stability(couple[0], couple[1])
-            and check_block_stability(couple[1], couple[0])
+            check_vertexes_stability(couple[0], couple[1])
+            and check_vertexes_stability(couple[1], couple[0])
         ):
             return False
     return True
 
 
+def is_stable_partition(partition: List[_QBlock]) -> bool:
+    return is_stable_vertexes_partition(
+        [list(block.vertexes) for block in partition]
+    )
+
+
 # return True if A_block \subseteq R^{-1}(B_block) or
 # A_block cap R^{-1}(B_block) = \emptyset
-def check_block_stability(
+def check_vertexes_stability(
     A_block_vertexes: Iterable[_Vertex], B_block_vertexes: Iterable[_Vertex]
 ) -> bool:
     """Checks the stability of the first block with respect to the second one.
@@ -58,3 +63,25 @@ def check_block_stability(
     # x \in E({x}) AND y \in B.
     # not any == True if the set "image of A" and B are distinct.
     return all(is_inside_B) or not any(is_inside_B)
+
+
+# return True if A_block \subseteq R^{-1}(B_block) or
+# A_block cap R^{-1}(B_block) = \emptyset
+def check_block_stability(A_block: _QBlock, B_block: _QBlock) -> bool:
+    """Checks the stability of the first block with respect to the second one.
+    The two inputs must be list of Vertex instances, and the relation which we
+    consider for the stability is a->b, where a,b are two vertexes.
+
+    Args:
+        A_block_vertexes (list[_Vertex]): The checked block.
+        B_block_vertexes (list[_Vertex]): The block against we check the
+            stability of A.
+
+    Returns:
+        bool: True if A is stable with respect to B. False otherwise.
+    """
+
+    A_block_vertexes = [vertex for vertex in A_block.vertexes]
+    B_block_vertexes = [vertex for vertex in B_block.vertexes]
+
+    return check_vertexes_stability(A_block_vertexes, B_block_vertexes)
