4fe2823b74615c0c0c8d6aae7d41b4e0e5498b75
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-02-12 00:19:20 +0100

    fix failing tests after last changes

--- tests/saha/test_saha.py
+++ tests/saha/test_saha.py
@@ -5,6 +5,7 @@ from bisimulation_algorithms.saha.graph_decorator import (
 )
 from bisimulation_algorithms.dovier_piazza_policriti.fba import (
     create_initial_partition,
+    build_block_counterimage,
 )
 from bisimulation_algorithms.saha.saha import (
     check_old_blocks_relation,
@@ -13,6 +14,9 @@ from bisimulation_algorithms.saha.saha import (
     propagate_wf,
     propagate_nwf,
     check_new_scc,
+    exists_causal_splitter,
+    both_blocks_go_to_block,
+    merge_condition,
 )
 from tests.fba.rank.rank_test_cases import graphs
 from bisimulation_algorithms.dovier_piazza_policriti.rank_computation import (
@@ -23,7 +27,14 @@ from .saha_test_cases import (
     new_scc_correct_value,
     new_scc_graphs,
     new_scc_new_edge,
+    exists_causal_splitter_qblocks,
+    exists_causal_splitter_result_map,
+    both_blocks_goto_result_map,
 )
+from tests.pta.pta_test_cases import graph_partition_rscp_tuples
+from bisimulation_algorithms.paige_tarjan.graph_decorator import initialize
+import itertools
+from bisimulation_algorithms.paige_tarjan.pta import pta
 
 
 def test_check_old_blocks_relation():
@@ -150,12 +161,138 @@ def test_check_new_scc_cleans(graph, new_edge, value):
         assert vertex.visited == False
 
 
-def test_exists_causal_splitter():
-    pass
+@pytest.mark.parametrize(
+    "qblocks, result_map",
+    zip(exists_causal_splitter_qblocks, both_blocks_goto_result_map),
+)
+def test_both_blocks_go_to_block(qblocks, result_map):
+    counterimages = [build_block_counterimage(block) for block in qblocks]
+
+    for ints, result in result_map:
+        block1, block2, block = ints
+        assert (
+            both_blocks_go_to_block(
+                qblocks[block1], qblocks[block2], counterimages[block]
+            )
+            == result
+        )
+
+
+@pytest.mark.parametrize(
+    "qblocks, result_map",
+    zip(exists_causal_splitter_qblocks, both_blocks_goto_result_map),
+)
+def test_both_blocks_go_to_block_commutative(qblocks, result_map):
+    counterimages = [build_block_counterimage(block) for block in qblocks]
+
+    for ints, result in result_map:
+        block1, block2, block = ints
+        assert both_blocks_go_to_block(
+            qblocks[block1], qblocks[block2], counterimages[block]
+        ) == both_blocks_go_to_block(
+            qblocks[block2], qblocks[block1], counterimages[block]
+        )
+
+
+@pytest.mark.parametrize(
+    "qblocks, result_map",
+    zip(exists_causal_splitter_qblocks, exists_causal_splitter_result_map),
+)
+def test_exists_causal_splitter(qblocks, result_map):
+    counterimages = [
+        (block, build_block_counterimage(block)) for block in qblocks
+    ]
+
+    for couple, result in result_map:
+        assert result == exists_causal_splitter(
+            qblocks[couple[0]], qblocks[couple[1]], counterimages
+        )
+
+
+@pytest.mark.parametrize(
+    "qblocks, result_map",
+    zip(exists_causal_splitter_qblocks, exists_causal_splitter_result_map),
+)
+def test_exists_causal_splitter_commutative(qblocks, result_map):
+    counterimages = [
+        (block, build_block_counterimage(block)) for block in qblocks
+    ]
+
+    for couple, _ in result_map:
+        assert exists_causal_splitter(
+            qblocks[couple[1]], qblocks[couple[0]], counterimages
+        ) == exists_causal_splitter(
+            qblocks[couple[0]], qblocks[couple[1]], counterimages
+        )
 
 
 def test_merge_condition():
-    pass
+    graph = nx.DiGraph()
+    graph.add_nodes_from(range(6))
+    graph.add_edges_from([(0, 1), (1, 2), (3, 1), (4, 6), (0, 6), (5, 6)])
+
+    qblocks, vertexes = initialize(graph, [(0, 1, 2, 3), (4, 5), (6,)])
+    rscp_qblocks = pta(qblocks)
+
+    node_to_qblock = [None for _ in graph.nodes]
+    for qb in rscp_qblocks:
+        for vertex in qb.vertexes:
+            node_to_qblock[vertex.label] = qb
+
+    blocks_and_counterimages = [
+        (block, build_block_counterimage(block)) for block in rscp_qblocks
+    ]
+
+    # can't merge because no same initial partition
+    assert not merge_condition(
+        node_to_qblock[2], node_to_qblock[6], blocks_and_counterimages
+    )
+
+    # different rank
+    assert not merge_condition(
+        node_to_qblock[2], node_to_qblock[1], blocks_and_counterimages
+    )
+
+    # same block
+    assert not merge_condition(
+        node_to_qblock[4], node_to_qblock[4], blocks_and_counterimages
+    )
+
+    # exists causal splitter
+    assert not merge_condition(
+        node_to_qblock[0], node_to_qblock[3], blocks_and_counterimages
+    )
+
+
+@pytest.mark.parametrize(
+    "graph, initial_partition",
+    map(lambda tp: (tp[0], tp[1]), graph_partition_rscp_tuples),
+)
+def test_merge_condition_with_initial_partition(graph, initial_partition):
+    qblocks, vertexes = initialize(graph, initial_partition)
+
+    blocks_and_counterimages = [
+        (block, build_block_counterimage(block)) for block in qblocks
+    ]
+
+    for tp in itertools.product(qblocks, qblocks):
+        assert not merge_condition(tp[0], tp[1], blocks_and_counterimages)
+
+
+@pytest.mark.parametrize(
+    "graph, initial_partition",
+    map(lambda tp: (tp[0], tp[1]), graph_partition_rscp_tuples),
+)
+def test_merge_condition_with_rscp(graph, initial_partition):
+    qblocks, vertexes = initialize(graph, initial_partition)
+    rscp = pta(qblocks)
+
+    blocks_and_counterimages = [
+        (block, build_block_counterimage(block)) for block in rscp
+    ]
+
+    for tp in itertools.product(rscp, rscp):
+        assert not merge_condition(tp[0], tp[1], blocks_and_counterimages)
 
 
 def test_recursive_merge():
